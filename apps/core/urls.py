9363aac8b8372fb2fbf1962f870d9f787e1153fd
Alice <aliceyang013@gmail.com>
2020-04-22 00:04:14 -0700

    created search, detailed search, and saved jobs pages

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,4 +5,7 @@ from apps.core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('search-results/', views.search_results, name='search_results'),
+    path('detailed-search-results/', views.detailed_search_results, name='detailed_search_results'),
+    path('saved-jobs/', views.saved_jobs, name='saved_jobs'),
 ]
