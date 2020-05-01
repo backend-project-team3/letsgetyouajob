df35aa0515796d4d9d8ec970d252e199a71af5b3
lwam-ng <lwam.ghebregergish@gmail.com>
2020-04-30 19:31:54 -0700

    Final fixing applied

--- apps/core/models.py
+++ apps/core/models.py
@@ -5,11 +5,11 @@ from apps.accounts.models import User
 class Saved_Job(models.Model):
     username = models.CharField(max_length=30)
     created = models.DateTimeField(auto_now_add=True)
-    job_id = models.CharField(max_length=50)
+    job_id = models.CharField(max_length=150)
     applied = models.BooleanField(default=False)
     job_title = models.CharField(max_length=50)
     job_description = models.TextField()
-
+    company_url = models.CharField(max_length=200)
 
 def __str__(self):
         return self.username + ' said ' + self.text
--- apps/core/urls.py
+++ apps/core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('search-results/', views.search_results, name='search_results'),
     path('detailed_search_results/<job_id>/', views.detailed_search_results, name='detailed_search_results'),
     path('saved_jobs/<user_name>/', views.saved_jobs, name='saved_jobs'),
-    path('save_job/<job_id>/<user_name>/', views.save_job, name='save_job')
-
+    path('save_job/<job_id>/<user_name>/', views.save_job, name='save_job'),
+    path('apply_job/<job_id>/<user_name>/', views.apply_job, name='apply_job')
+    
 ]
